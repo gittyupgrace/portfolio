7b1b68d14df4fe98001d6d362788b02c8e451061
Grace <gracecmccaffrey@gmail.com>
2022-11-23 17:50:56 -0500

    added initial structure for projects array in js file

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ app.skills = [
     {
         name: 'Web Accessibility',
         icon: ['fa-solid', 'fa-universal-access']
-    },
+    }
 ];
 
 app.displaySkills = () => {
@@ -62,6 +62,56 @@ app.displaySkills = () => {
         skillsList.append(skillsListItem);
     });
 };
+
+
+//Work Section
+
+app.projects = [
+    {
+        title: 'Postcards from Ron',
+        image: '',
+        alt: '',
+        tools: ['html', 'css', 'js'], 
+        description: '', 
+        links: ['url', 'url']
+    },
+    {
+        title: 'Block Party: A Quiz About Quilts',
+        image: '', 
+        alt: '', 
+        tools: ['html', 'css', 'js'],
+        description: '',
+        links: ['url', 'url']
+    },
+    {
+        title: 'Sophie',
+        image: '', 
+        alt: '', 
+        tools: ['html', 'css', 'js'],
+        description: '',
+        links: ['url', 'url']
+    },
+    {
+        title: 'React Project',
+        image: '',
+        alt: '', 
+        tools: ['html', 'css', 'react'],
+        description: '',
+        links: ['url', 'url']
+    },
+]
+
+//loop through array and for each project:
+
+//insert below info into .projectContainer div --> should this be a ul with .projectContainer being a li instead? maybe!
+
+//inside .projectImage div - populate img src and alt text
+//inside .projectContent div -
+//populate h5 element with title
+//get ul, create li's, populate with tools by looping through tool array
+//get p, populate with description
+//get ul, create li's with a tags, populate href with link urls by looping through link array
+
 
 app.init = () => {
     app.displaySkills();
