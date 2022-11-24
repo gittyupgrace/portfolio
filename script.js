1680c50f35bb7c93303f2fad8416864f7ff43ad1
Grace <gracecmccaffrey@gmail.com>
2022-11-24 11:48:39 -0500

    completed js logic for work section

--- script.js
+++ script.js
@@ -43,18 +43,16 @@ app.skills = [
 ];
 
 app.displaySkills = () => {
-    app.skillsList = document.getElementById('skillsList');
+    const skillsList = document.getElementById('skillsList');
 
     app.skills.forEach((skill) => {
         const skillsListItem = document.createElement('li');
 
         const skillsIcon = document.createElement('i');
 
-        const iconClassArray = skill.icon;
-        iconClassArray.forEach((iconClass) => {
+        skill.icon.forEach((iconClass) => {
             skillsIcon.classList.add('icon');
             skillsIcon.classList.add(iconClass);
-            console.log(skillsIcon)
         });
 
         skillsListItem.textContent = skill.name;
@@ -69,52 +67,137 @@ app.displaySkills = () => {
 app.projects = [
     {
         title: 'Postcards from Ron',
-        image: '',
-        alt: '',
+        image: './assets/mockupTestImg.png',
+        alt: 'this is alt text placeholder',
         tools: ['html', 'css', 'js'], 
-        description: '', 
-        links: ['url', 'url']
+        description: `Postcards from Ron is an app that pulls data from two separate API's to create a charming and giggle-inducing visual experience for fans of the character Ron Swanson from TV show Parks and Recreation.`, 
+        links: [
+            {
+                title: 'View Live', 
+                href: 'url'
+            }, 
+            {
+                title: 'View Code',
+                href: 'https://github.com/gittyupgrace/postcards-from-ron'
+            }]
     },
     {
         title: 'Block Party: A Quiz About Quilts',
-        image: '', 
+        image: './assets/mockupTestImg.png', 
         alt: '', 
         tools: ['html', 'css', 'js'],
-        description: '',
-        links: ['url', 'url']
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt possimus ipsam eveniet ex quasi unde laboriosam numquam minus soluta voluptates?',
+        links: [
+            {
+                title: 'View Live',
+                href: 'url'
+            },
+            {
+                title: 'View Code',
+                href: 'url'
+            }]
     },
     {
         title: 'Sophie',
-        image: '', 
+        image: './assets/mockupTestImg.png', 
         alt: '', 
         tools: ['html', 'css', 'js'],
-        description: '',
-        links: ['url', 'url']
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt possimus ipsam eveniet ex quasi unde laboriosam numquam minus soluta voluptates?',
+        links: [
+            {
+                title: 'View Live',
+                href: 'url'
+            },
+            {
+                title: 'View Code',
+                href: 'url'
+            }]
     },
     {
         title: 'React Project',
-        image: '',
+        image: './assets/mockupTestImg.png',
         alt: '', 
         tools: ['html', 'css', 'react'],
-        description: '',
-        links: ['url', 'url']
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt possimus ipsam eveniet ex quasi unde laboriosam numquam minus soluta voluptates?',
+        links: [
+            {
+                title: 'View Live',
+                href: 'url'
+            },
+            {
+                title: 'View Code',
+                href: 'url'
+            }]
     },
 ]
 
-//loop through array and for each project:
+app.displayProjects = () => {
 
-//insert below info into .projectContainer div --> should this be a ul with .projectContainer being a li instead? maybe!
+    const projectList = document.getElementById('projectList');
 
-//inside .projectImage div - populate img src and alt text
-//inside .projectContent div -
-//populate h5 element with title
-//get ul, create li's, populate with tools by looping through tool array
-//get p, populate with description
-//get ul, create li's with a tags, populate href with link urls by looping through link array
+    app.projects.forEach((project) => {
 
+        //for each project object, create an li with a class of projectContainer
+        const projectListItem = document.createElement('li');
+        projectListItem.classList.add('projectContainer');
+
+        //for inside projectContainer li, create two divs, one with a class of projectImage, the other with a class of project Content, append each to li
+        const projectImageContainer = document.createElement('div');
+        projectImageContainer.classList.add('projectImage');
+        projectListItem.appendChild(projectImageContainer);
+
+        const projectContentContainer = document.createElement('div');
+        projectContentContainer.classList.add('projectContent');
+        projectListItem.appendChild(projectContentContainer);
+
+        console.log(projectListItem);
+
+        //projectImageContainer div --> create img element, insert img src and alt text, append img element to imagecontainer div
+        const image = document.createElement('img');
+        image.src = project.image;
+        image.alt = project.alt;
+        projectImageContainer.append(image);
+
+
+        //projectContentContainer div --> create and populate h5, tool ul, p, links ul and append to contentcontainer div
+
+        const projectTitle = document.createElement('h5');
+        projectTitle.textContent = project.title;
+        projectContentContainer.append(projectTitle);
+
+        const toolList = document.createElement('ul');
+        project.tools.forEach((tool) => {
+            const toolListItem = document.createElement('li');
+            toolListItem.textContent = tool;
+            toolList.append(toolListItem);
+        });
+        projectContentContainer.append(toolList);
+
+        const descriptionContainer = document.createElement('p');
+        descriptionContainer.textContent = project.description;
+        projectContentContainer.append(descriptionContainer);
+
+        const linkList = document.createElement('ul');
+        project.links.forEach((link) => {
+            const linkListItem = document.createElement('li');
+            const linkUrl = document.createElement('a');
+
+            linkUrl.textContent = link.title;
+            linkUrl.href = link.href;
+
+            linkListItem.append(linkUrl);
+            linkList.append(linkListItem);
+        });
+        projectContentContainer.append(linkList);
+
+        //finally append projectListItem to projectList ul
+        projectList.append(projectListItem);
+    });
+};
 
 app.init = () => {
     app.displaySkills();
+    app.displayProjects();
 }
 
 app.init();
